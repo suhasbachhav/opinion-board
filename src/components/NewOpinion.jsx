d63c379e750b3275741fcf795472462e114cb414
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-22 21:29:58 +0530

    formstatus

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,5 +1,6 @@
 import { useActionState, use } from "react";
 import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit";
 
 export function NewOpinion() {
 
@@ -74,9 +75,7 @@ export function NewOpinion() {
           </ul>
         )}
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        <Submit />
       </form>
     </div>
   );
